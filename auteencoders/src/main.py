e711b54b51e6af9c04bf758319084e7a4e837400
JRogulski <106926091+JRogulski@users.noreply.github.com>
2022-10-30 15:10:24 +0100

    Delete auteencoders directory

--- auteencoders/src/main.py
+++
@@ -1,5 +0,0 @@
-import torch
-from data_preprocessing import return_dataloaders
-
-train_dataloader, val_dataloader, test_dataloader = return_dataloaders(128, 128, 3)
-print(next(iter(train_dataloader)))
